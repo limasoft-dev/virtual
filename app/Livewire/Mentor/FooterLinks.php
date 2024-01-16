974c4afd03507730d54285a089d08c2c25da2044
AAndrade <aandrade@limasoft.pt>
2024-01-16 09:00:31 +0000

    decompor template e social links

---
+++ app/Livewire/Mentor/FooterLinks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Mentor;
+
+use Livewire\Component;
+
+class FooterLinks extends Component
+{
+    public function render()
+    {
+        return view('livewire.mentor.footer-links');
+    }
+}
